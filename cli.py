986aa580ae92c9fcf674b1c0982becf07f9e510b
byronamh <byronamh@gmail.com>
2018-06-05 01:42:24 -0600

    ffs

--- cli.py
+++ cli.py
@@ -63,7 +63,7 @@ def main(args):
 
 
 def generateData(days):
-    cur.execute('SELECT * FROM invoice ORDER BY invoiceId DESC LIMIT 1')
+    cur.execute('SELECT "invoiceId","customerID" FROM invoice ORDER BY invoiceId DESC LIMIT 1')
 
     # display the PostgreSQL database server version
     data = cur.fetchone()
